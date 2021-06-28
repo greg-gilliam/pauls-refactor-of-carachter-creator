a1a90455021ef5e1e4c4a0cb71d6b4847eb5e182
Paul Brubaker <pbrubak@gmail.com>
2021-06-28 13:43:47 -0700

    fix eslint mistakes

--- app.js
+++ app.js
@@ -22,30 +22,30 @@ let errorMessage = document.getElementById('error-message');
 let submitValues = document.getElementById('submit-values');
 
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('change', () =>{
-    allottedSoFarValue = (parseInt(strength.value) || 0)
-                        + (parseInt(speed.value) || 0)
-                        + (parseInt(intelligence.value) || 0)
-                        + (parseInt(empathy.value) || 0)
-                        + (parseInt(luck.value) || 0);
-    allottedSoFarDiv.textContent = `You have allotted ${allottedSoFarValue} points so far`;
-  });
-}
+    inputs[i].addEventListener('change', () => {
+        allottedSoFarValue = (parseInt(strengthValue.value) || 0)
+                          + (parseInt(speedValue.value) || 0)
+                          + (parseInt(intelligenceValue.value) || 0)
+                          + (parseInt(empathyValue.value) || 0)
+                          + (parseInt(luckValue.value) || 0);
+        allottedSoFarDiv.textContent = `You have allotted ${allottedSoFarValue} points so far`;
+      });
+  }
 
 submitValues.addEventListener('click', () => {
-  if (allottedSoFarValue < 50) {
-    errorMessage.textContent = `Uh oh! You haven't allotted all your points yet!`
-  } else if (allottedSoFarValue > 50) {
-    errorMessage.textContent = `Oh no! You've allotted too many points! Time to make some tough decisions.`
-  } else {
-    strengthImage.src = getStrength(strength.value);
-    speedImage.src = getSpeed(speed.value);
-    intelligenceImage.src = getIntelligence(intelligence.value);
-    empathyImage.src = getEmpathy(empathy.value);
-    luckImage.src = getLuck(luck.value);
-    errorMessage.textContent = '';
-  }
-})
+    if (allottedSoFarValue < 50) {
+        errorMessage.textContent = `Uh oh! You haven't allotted all your points yet!`;
+    } else if (allottedSoFarValue > 50) {
+        errorMessage.textContent = `Oh no! You've allotted too many points! Time to make some tough decisions.`;
+    } else {
+        strengthImage.src = getStrength(strength.value);
+        speedImage.src = getSpeed(speed.value);
+        intelligenceImage.src = getIntelligence(intelligence.value);
+        empathyImage.src = getEmpathy(empathy.value);
+        luckImage.src = getLuck(luck.value);
+        errorMessage.textContent = '';
+    }
+});
 
   // get user input(s)
   // do any needed work with the value(s)
