ab6f4b21b65525929a3d2898e08698d83ce1e394
Paul Brubaker <pbrubak@gmail.com>
2021-06-28 13:23:43 -0700

    implement basic functionality

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 // import functions
+import { getStrength, getSpeed, getIntelligence, getEmpathy, getLuck } from './get-images.js';
 
 // reference needed DOM elements
+let strengthValue = document.getElementById('strength');
+let speedValue = document.getElementById('speed');
+let intelligenceValue = document.getElementById('intelligence');
+let empathyValue = document.getElementById('empathy');
+let luckValue = document.getElementById('luck');
 
+let strengthImage = document.getElementById('strength-image');
+let speedImage = document.getElementById('speed-image');
+let intelligenceImage = document.getElementById('intelligence-image');
+let empathyImage = document.getElementById('empathy-image');
+let luckImage = document.getElementById('luck-image');
 
-// set event listeners 
+let inputs = Array.from(document.getElementsByTagName('input'));
+let allottedSoFarValue;
+let allottedSoFarDiv = document.getElementById('allotted-so-far');
+let errorMessage = document.getElementById('error-message');
+
+let submitValues = document.getElementById('submit-values');
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('change', () =>{
+    allottedSoFarValue = (parseInt(strength.value) || 0)
+                        + (parseInt(speed.value) || 0)
+                        + (parseInt(intelligence.value) || 0)
+                        + (parseInt(empathy.value) || 0)
+                        + (parseInt(luck.value) || 0);
+    allottedSoFarDiv.textContent = `You have allotted ${allottedSoFarValue} points so far`;
+  });
+}
+
+submitValues.addEventListener('click', () => {
+  if (allottedSoFarValue < 50) {
+    errorMessage.textContent = `Uh oh! You haven't allotted all your points yet!`
+  } else if (allottedSoFarValue > 50) {
+    errorMessage.textContent = `Oh no! You've allotted too many points! Time to make some tough decisions.`
+  } else {
+    strengthImage.src = getStrength(strength.value);
+    speedImage.src = getSpeed(speed.value);
+    intelligenceImage.src = getIntelligence(intelligence.value);
+    empathyImage.src = getEmpathy(empathy.value);
+    luckImage.src = getLuck(luck.value);
+    errorMessage.textContent = '';
+  }
+})
+
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
---
+++ get-images.js
@@ -0,0 +1,49 @@
+export function getStrength(int) {
+    if (int <= 7) {
+        return './assets/llama.svg';
+    } else if (int <= 14) {
+        return './assets/tiger.svg';
+    } else {
+        return './assets/bear.svg';
+    }
+}
+
+export function getSpeed(int) {
+    if (int <= 7) {
+        return './assets/llama.svg';
+    } else if (int <= 14) {
+        return './assets/cow.svg';
+    } else {
+        return './assets/tiger.svg';
+    }
+}
+
+export function getIntelligence(int) {
+    if (int <= 7) {
+        return './assets/llama.svg';
+    } else if (int <= 14) {
+        return './assets/rat.svg';
+    } else {
+        return './assets/elephant.svg';
+    }
+}
+
+export function getEmpathy(int) {
+    if (int <= 7) {
+        return './assets/llama.svg';
+    } else if (int <= 14) {
+        return './assets/penguin.svg';
+    } else {
+        return './assets/cow.svg';
+    }
+}
+
+export function getLuck(int) {
+    if (int <= 7) {
+        return './assets/llama.svg';
+    } else if (int <= 14) {
+        return './assets/wolf.svg';
+    } else {
+        return './assets/rat.svg';
+    }
+}
